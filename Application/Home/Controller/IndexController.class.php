e186b7a902c926fcda166848c893a86a5679b0a5
wuhao <648820957@qq.com>
2015-01-13 16:58:41 +0800

    完善中

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,8 +1,47 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+/**
+ * 网站首页
+ * @author Administrator
+ *
+ */
 class IndexController extends Controller {
-    public function index(){
+    function index(){
+        $this->_search();
+    }
+    
+    function s(){
+        $this->_search();
+    }
+    
+    /**
+     * 搜索
+     */
+    function _search(){
+        $model=D('Admin/Article');
+        $where['isDel']=0;
+        $categoryID=I('get.categoryID');
+        if(!empty($categoryID)){
+            $where['categoryID']=$categoryID;
+        }
+        else{
+            $categoryID=0;
+        }
+        
+        $result = $model->where($where)
+                        ->order('sortID asc,createTime desc')
+                        ->select();
+        
+        $count = $model->where($where)->count();
+        
+        $categoryArray = D('Admin/ArticleCategory')->getList(0);
+        
+        $this->assign('categoryID', $categoryID)
+             ->assign('count', $count)
+             ->assign('result', $result)
+             ->assign('categoryArray', $categoryArray);
+        
         $this->display('index');
     }
 }
